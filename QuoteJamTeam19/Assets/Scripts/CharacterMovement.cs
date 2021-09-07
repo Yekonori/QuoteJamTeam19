b41e64e5df5e859201ed003fdf78d87719927d86
Cocomineral <tintinch59@gmail.com>
2021-09-07 14:34:34 +0100

    - Added Character movement script - Added Game Scene

---
+++ QuoteJamTeam19/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class CharacterMovement : MonoBehaviour
+{
+    private BoxCollider2D boxcollider;
+    private Rigidbody2D rigidB;
+    private float moveY;
+    private Vector2 movement;
+
+    public float verticalSpeed;
+    public float horizontalSpeed;
+    public bool canRun;
+
+    private void Start()
+    {
+        boxcollider = GetComponent<BoxCollider2D>();
+        rigidB = GetComponent<Rigidbody2D>();
+        canRun = true;
+    }
+
+    private void Update()
+    {
+        if (canRun)
+        {
+            moveY = Input.GetAxis("Vertical");
+            movement = new Vector2(horizontalSpeed, moveY);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidB.velocity = movement * verticalSpeed * Time.fixedDeltaTime;
+    }
+}
