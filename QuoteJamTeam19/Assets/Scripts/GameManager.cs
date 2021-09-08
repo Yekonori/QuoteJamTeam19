9f86c57923377431f06170ae170f25a835429c56
Guillaumin <noeguillaumin@gmail.com>
2021-09-08 14:00:00 +0200

    -Added PauseMenu prefab

--- QuoteJamTeam19/Assets/Scripts/GameManager.cs
+++ QuoteJamTeam19/Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     private GameManager() { }
     public static GameManager Instance { get { return instance; } }
     public CharacterMovement player;
+    public GameObject PauseMenuObject;
+    public bool isGamePaused = false;
 
     void Awake()
     {
@@ -18,6 +20,29 @@ public class GameManager : MonoBehaviour
 
         if (player == null)
             Debug.LogError("Player is not assigned");
+
+        if (PauseMenuObject.activeSelf == true && isGamePaused == false)
+            PauseMenuObject.SetActive(false);
+    }
+
+     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isGamePaused = !isGamePaused;
+            print(isGamePaused);
+        }
+
+        if (isGamePaused)
+        {
+            Time.timeScale = 0;
+            PauseMenuObject.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            PauseMenuObject.SetActive(false);
+        }
     }
 
     public void Lose()
