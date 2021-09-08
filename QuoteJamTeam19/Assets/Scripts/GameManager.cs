b96198b0939d0918c91f80ad6161ffe2886f41ad
Yekonori <yannisdemacedo@gmail.com>
2021-09-08 14:47:31 +0200

    - Added LeveScene2 - Added change level scene on ring - Added Ring tag

--- QuoteJamTeam19/Assets/Scripts/GameManager.cs
+++ QuoteJamTeam19/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,12 +13,27 @@ public class GameManager : MonoBehaviour
     public Timer timer;
     public bool canPlay = true;
 
+    private bool dignityToSet = false;
+    public bool DignityToSet { get { return dignityToSet; } }
+
+    private float dignityLeft = 0;
+    public float DignityLeft { get { return dignityLeft; } }
+
+    private bool timerToSet = false;
+    public bool TimerToSet { get { return timerToSet; } }
+
+    private int minutsLeft = 0;
+    public int MinutsLeft { get { return minutsLeft; } }
+    private int secondsLeft = 0;
+    public int SecondsLeft { get { return secondsLeft; } }
+
     void Awake()
     {
-        if (instance != null && instance != this)
-            Destroy(gameObject);
-
-        instance = this;
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
 
         if (player == null)
             Debug.LogError("Player is not assigned");
@@ -28,5 +45,25 @@ public class GameManager : MonoBehaviour
 
         player.StopPlayer();
         timer.HardStopTimer();
+    }
+
+    public void GetRing()
+    {
+        dignityToSet = true;
+        dignityLeft = DignityBar.Instance.GetDignityValue();
+
+        timerToSet = true;
+        minutsLeft = timer.GetMinuts();
+        secondsLeft = timer.GetSeconds();
+
+        timer.HardStopTimer();
+        player.StopPlayer();
+        // On load la scene suivante
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Victory()
+    {
+
     }
 }
