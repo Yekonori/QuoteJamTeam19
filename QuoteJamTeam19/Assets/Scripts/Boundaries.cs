6da331e92c3eb87725c6ed00a93cb119e4e05edc
Cocomineral <tintinch59@gmail.com>
2021-09-09 11:05:51 +0100

    - Added Boundaries when going left to right - Bug when going right to left

--- QuoteJamTeam19/Assets/Scripts/Boundaries.cs
+++ QuoteJamTeam19/Assets/Scripts/Boundaries.cs
@@ -4,26 +4,50 @@ using UnityEngine;
 
 public class Boundaries : MonoBehaviour
 {
-    private Camera MainCamera;
     private Vector2 screenBounds;
     private float objectWidth;
     private float objectHeight;
+    private Camera mainCamera;
+    private Vector3 viewPos;
 
-    // Use this for initialization
     void Start()
     {
-        MainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
-    }
-
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        if (mainCamera != Camera.main)
+        {
+            mainCamera = Camera.main;
+            Debug.Log("Camera Switched");
+        }
         objectWidth = transform.GetComponentInChildren<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
         objectHeight = transform.GetComponentInChildren<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
-        Vector3 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
-        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+    }
+
+    void LateUpdate()
+    {
+        if(mainCamera != Camera.main)
+        {
+            mainCamera = Camera.main;
+            Debug.Log("Camera Switched");
+        }
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+        viewPos = transform.position;
+        if (!GameManager.Instance.hasRing)
+        {
+            viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+            viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        }
+        else
+        {
+
+        }
+        
         transform.position = viewPos;
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(viewPos, screenBounds);
     }
 }
